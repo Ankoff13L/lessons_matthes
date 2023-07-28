75ffb733f980d4bc8a918b6eac6ec78653ddfdae
Andrei Kuznecov <ankoff13@gmail.com>
2023-07-28 18:36:06 +0300

    file greeter

---
+++ greeter.py
@@ -0,0 +1,12 @@
+# def display_message():
+#     print ("Hello!")
+
+# display_message()
+
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+favorite_book('Alice in Wonderland')
+
+
