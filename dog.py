5b9fefe87dfba51cad166d4f4de991d0eabfeb15
Andrei Kuznecov <ankoff13@gmail.com>
2023-08-09 22:08:17 +0300

    upgrade

---
+++ dog.py
@@ -0,0 +1,34 @@
+
+class Dog():
+    """Простая модель собаки"""
+
+    def __init__(self, name, age):
+        """"Инициализирует атрибуты name и age."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """"Собака садится по команде."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Собака перекатывается по команде"""
+        print(f"{self.name} rolled over!")
+
+
+my_dog = Dog('willie', 6)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} yers old.")
+my_dog.sit()
+my_dog.roll_over()
+
+
+your_dog = Dog('lucy', 3)
+
+print(f"\nMy dog's name is {your_dog.name}")
+print(f"My dog is {your_dog.age} yers old.")
+your_dog.sit()
+
+
+
