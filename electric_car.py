b4d9bae3e9c8bece21f63d85aea9f4c31ea98244
Andrei Kuznecov <ankoff13@gmail.com>
2023-08-17 00:09:58 +0300

    upgrade

--- electric_car.py
+++ electric_car.py
@@ -36,6 +36,31 @@ class Car():
         self.odometer_reading += miles
 
 
+class Battery():
+    """Простая модель аккумулятора автомобиля."""
+
+
+    def __init__(self, battery_size=75):
+        """Инициализирует атрибуты аккумулятора."""
+        self.battery_size = battery_size
+
+
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+
+    def get_range(self):
+        """Выводит приблизительный запас ходя для аккумулятора."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} milles on a full charge.")
+
+
+
 class ElectricCar(Car):
     """
     Инициализирует атрибуты класса-родителя.
@@ -45,17 +70,18 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Инициализирует атрибуты класса-родителя."""
         super().__init__(make, model, year)
-        self.battery_size = 75
-
-
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+        self.battery = Battery()
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
+
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
+
+
+
 
 
 
--- restaurants.py
+++ restaurants.py
@@ -93,46 +93,168 @@
 
 
 
-class User():
+# class User():
 
-    def __init__(self, first_name, last_name, age):
-        self.f_name = first_name
-        self.l_name = last_name
-        self.age = age
-        self.login_attemps = 0
-
-
-    def describe_user(self):
-        print(f"User: {self.f_name.title()} {self.l_name.title()}, {self.age} age. ")
-
-    def greet_user(self):
-        print(f"Hello {self.f_name.title()}!")
-
-    def increment_login_attemps(self):
-        """Увеличивает попытки входа на 1."""
-        self.login_attemps += 1
-
-    def reset_login_attemps(self):
-        """Обнуляет значение попыток входа (login_attemps)"""
-        self.login_attemps -= self.login_attemps
-
-    def number_attemps(self):
-        """Сколько раз входили"""
-        print(f"Number of entries in the program: {self.login_attemps}.")
+#     def __init__(self, first_name, last_name, age):
+#         self.f_name = first_name
+#         self.l_name = last_name
+#         self.age = age
+#         self.login_attemps = 0
 
 
+#     def describe_user(self):
+#         print(f"User: {self.f_name.title()} {self.l_name.title()}, {self.age} age. ")
+
+#     def greet_user(self):
+#         print(f"Hello {self.f_name.title()}!")
+
+#     def increment_login_attemps(self):
+#         """Увеличивает попытки входа на 1."""
+#         self.login_attemps += 1
+
+#     def reset_login_attemps(self):
+#         """Обнуляет значение попыток входа (login_attemps)"""
+#         self.login_attemps -= self.login_attemps
+
+#     def number_attemps(self):
+#         """Сколько раз входили"""
+#         print(f"Number of entries in the program: {self.login_attemps}.")
+
+
+
+# # user_1 = User('anwei', 'smit', 31)
+# # user_1.describe_user()
+# # user_1.greet_user()
 
 # user_1 = User('anwei', 'smit', 31)
-# user_1.describe_user()
-# user_1.greet_user()
+# user_1.increment_login_attemps()
+# user_1.increment_login_attemps()
+# user_1.increment_login_attemps()
+# user_1.number_attemps()
 
-user_1 = User('anwei', 'smit', 31)
-user_1.increment_login_attemps()
-user_1.increment_login_attemps()
-user_1.increment_login_attemps()
-user_1.number_attemps()
+# user_1.reset_login_attemps()
+# user_1.number_attemps()
 
-user_1.reset_login_attemps()
-user_1.number_attemps()
 
+
+
+# class Restaurant():
+
+#     def __init__(self, restaurant_name, cuisine_type):
+#         self.rest_name = restaurant_name
+#         self.cuisi_type = cuisine_type
+#         self.number_served = 0
+
+#     def set_number_served(self, num_served):
+#         """Позваляет задать количество обслуженных посетителей."""
+#         self.number_served = num_served
+
+#     def number_visitors(self):
+#         """Выводит количество посетителей"""
+#         print(f"\n{self.number_served} visitors served in total.")
+
+#     def increment_number_served(self, number):
+#         """Увеличивает количество обслуженных посетителей."""
+#         self.number_served += number
+
+#     def describe_restaurant(self):
+#         print("\nIt's a trendy restaurant.")
+#         print("You can have a delicious meal there.")
+
+#     def open_restaurant(self):
+#         print('Restaurant Open!')
+
+
+
+# class IceCreamStand(Restaurant):
+#     """Опрелеляется класс-потомок класса-родителя Restaurant."""
+#     def __init__(self, restaurant_name, cuisine_type):
+#         """Инициализирует атрибуты класса-родителя."""
+#         super().__init__(restaurant_name, cuisine_type)
+#         self.flavors = []
+
+
+#     def flavors_name(self):
+#         """Выводит список вкусов мороженого."""
+#         print(self.flavors)
+
+
+
+# my_flavors = IceCreamStand('brazier', 'fish')
+# my_flavors.flavors_name()
+
+
+
+
+
+
+
+
+
+# class User():
+
+#     def __init__(self, first_name, last_name, age):
+#         self.f_name = first_name
+#         self.l_name = last_name
+#         self.age = age
+#         self.login_attemps = 0
+
+
+#     def describe_user(self):
+#         print(f"User: {self.f_name.title()} {self.l_name.title()}, {self.age} age. ")
+
+#     def greet_user(self):
+#         print(f"Hello {self.f_name.title()}!")
+
+#     def increment_login_attemps(self):
+#         """Увеличивает попытки входа на 1."""
+#         self.login_attemps += 1
+
+#     def reset_login_attemps(self):
+#         """Обнуляет значение попыток входа (login_attemps)"""
+#         self.login_attemps -= self.login_attemps
+
+#     def number_attemps(self):
+#         """Сколько раз входили"""
+#         print(f"Number of entries in the program: {self.login_attemps}.")
+
+
+# class Admin(User):
+#     def __init__(self, first_name, last_name, age):
+#         super().__init__(first_name, last_name, age)
+#         self.privileges = [
+#             'разрешено добавлять сообщения',
+#             'разрешено удалять пользователей',
+#             'разрешено банить пользователей',
+#         ]
+
+
+#     def show_privileges(self):
+#         """Выводит набор привелегий админа."""
+#         for privileg in self.privileges():
+#             print(privileg)
+
+# my_admin = Admin('anwei', 'smit', 31)
+# my_admin.show_privileges()
+
+
+
+
+class Privileges():
+    def __init__(self, ):
+        self.privileges = [
+            'разрешено добавлять сообщения',
+            'разрешено удалять пользователей',
+            'разрешено банить пользователей',
+        ]
+
+
+    def show_privileges(self):
+        """Выводит набор привелегий админа."""
+        for privileg in self.privileges():
+            print(privileg)
+
+
+my_privileges = Privileges()
+my_privileges.show_privileges()
 
