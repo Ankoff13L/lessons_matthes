16b0d5ff7102e8c5f804343884e24b00443d893c
Andrei Kuznecov <ankoff13@gmail.com>
2023-09-07 00:26:38 +0300

    upgrade

--- alice.py
+++ alice.py
@@ -4,6 +4,15 @@
 #     contents = f.read()
 
 
+# filename = 'alice.txt'
+
+# try:
+#     with open(filename, encoding='utf-8') as f:
+#         contents = f.read()
+# except FileNotFoundError:
+#     print(f"Sorry, the file {filename} does not exict.")
+
+
 filename = 'alice.txt'
 
 try:
@@ -11,6 +20,10 @@ try:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry, the file {filename} does not exict.")
-
+else:
+    # Подсчет приблизительного количества строк в файле.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {num_words} words.")
 
 
